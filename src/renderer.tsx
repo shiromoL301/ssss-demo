b43109fd8a95d3b3ebf002cc25053217623de70f
syakoo <nichijou4009@gmail.com>
2021-06-20 15:18:46 +0900

    chore: apply eslint.

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { VFC } from 'react'
 import ReactDOM from 'react-dom'
 
-const App: React.VFC = () => <h1>Hello Electron!</h1>
+const App: VFC = () => <h1>Hello Electron!</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'))
