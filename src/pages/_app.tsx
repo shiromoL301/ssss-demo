818638223a757d4ab12084105f665816daf0188e
syakoo <nichijou4009@gmail.com>
2021-06-24 21:50:33 +0900

    fix: add icon link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       <title>SSSS Demo</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" type="image/png" href="/logo_512.png" />
+      <link rel="icon" type="image/png" href="/ssss-demo/logo_512.png" />
     </Head>
     <MainLayout>
       <Component {...pageProps} />
